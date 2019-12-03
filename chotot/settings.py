dcb632416ff473d0465dd5919939f582d33eeb1c
Long Nguyen <longnguyen@hhdgroup.com>
2019-12-03 22:34:25 +0700

    test12

--- chotot/settings.py
+++ chotot/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/timraovat'
 LOGOUT_REDIRECT_URL = '/'
-ALLOWED_HOSTS = ['***.herokuapp.com']
+ALLOWED_HOSTS = ['still-depths-49162.herokuapp.com']
 
 
 # Application definition
