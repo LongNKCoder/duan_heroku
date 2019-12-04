3c550fb993a4829ed0d65c5740e78716993d7731
Long Nguyen <longnguyen@hhdgroup.com>
2019-12-04 22:10:40 +0700

    test_lan2

--- chotot/settings.py
+++ chotot/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/timraovat'
 LOGOUT_REDIRECT_URL = '/'
-ALLOWED_HOSTS = ['fierce-reef-10134.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['quiet-citadel-98654.herokuapp.com','127.0.0.1']
 
 
 # Application definition
