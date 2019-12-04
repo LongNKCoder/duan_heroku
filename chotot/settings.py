0e32f0a45184e2f9b988bda72a3da7eb8d5149dd
Long Nguyen <longnguyen@hhdgroup.com>
2019-12-04 20:23:51 +0700

    25

--- chotot/settings.py
+++ chotot/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/timraovat'
 LOGOUT_REDIRECT_URL = '/'
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
+ALLOWED_HOSTS = ['fierce-reef-10134','127.0.0.1']
 
 
 # Application definition
